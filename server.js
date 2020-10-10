98786caf0fc281a4a1cc4456a4da23494da739b6
Aalyousif <aalyousif@deakin.edu.au>
2020-10-10 17:11:00 +0300

    update on server js

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-const express = require("express");
+// const express = require("express");
 
-const path = require("path");
+// const path = require("path");
 
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, "build")));
+
+// app.post("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
+// app.listen(process.env.PORT || 3000);
+
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.post("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => console.log(`Listening on port ${port}`));
